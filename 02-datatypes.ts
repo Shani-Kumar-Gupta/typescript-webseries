22e6b74db6405e392cb8c1db3da8e5c3932d2d77
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-05-25 23:17:40 +0530

    Data Types: Number, String, Boolean and Truthy and Falsy values

---
+++ 02-datatypes.ts
@@ -0,0 +1,28 @@
+/*
+@: DATATYPES IN TYPESCRIPT
+  - Every variable in Typecript is having certain datatypes in place basis on the value assigned to it.
+  @: DataTypes in TS-
+    - String
+    - Number
+    - Boolean
+*/
+
+// 1. String
+const str1 = 'This is a string created using single quotes';
+const str2 = "This is a string created using double quotes";
+const str3 = `This is a string created using back ticks`;
+
+// 2. Number
+let num1 = 300; // TypeScript number saved as floating point in memory. - 300.0
+let num2 = 23.45;
+let pi = 3.14;
+
+// 3. Boolean
+let isString = true;
+let isCorrect = false;
+let isEqual = true;
+
+let isGreater = 10 > 15;
+
+
+
