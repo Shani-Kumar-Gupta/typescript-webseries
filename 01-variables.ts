08f0d7e163e8211f7fba31e985bf81341af1c094
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-05-25 23:01:00 +0530

    Variable declaration and initialization with naming convensions

---
+++ 01-variables.ts
@@ -0,0 +1,18 @@
+/*
+The variable is like a named storage in which we can store a value and we can use it again and again in our codebase.
+    @: VARIABLES IN TYPESCRIPT
+    @: To declare a variable in TypeScript, we use var, let and const
+    @: (Important)
+      @: Variables infer the datatypes at the time of initialization only
+      @: Naming Convensions - It should be alphanumeric with _ and $ sign
+        - It should not start with numbers.
+        - It can start with _ or $.
+        - Variables name are case sensitive.
+        - Can't not use the reserved keyword as Variable name
+*/
+
+let num = 300;
+// let num = 200;
+let NUM = 200;
+
+const str = 'Hello! World';
