23e99184aa12cf26b33dcae57483f789b8adbf5a
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-05-26 15:14:18 +0530

    any data type in TS with example

---
+++ 08-anytype.ts
@@ -0,0 +1,20 @@
+/****************************************************
+ * ANY TYPE IN TYPESCRIPT
+ * 
+*/
+
+let dynamicData: any = 23;
+dynamicData = 'Shani';
+dynamicData = true;
+dynamicData = ['Shani', 'Aman', 'Riya'];
+dynamicData = [2, 45, 67, 89];
+dynamicData = {
+  fullName: 'Shani',
+  age: 25
+};
+
+let arr: any[] = [124, 'Shani', true, null, undefined, 'Hey'];
+
+let test;
+console.log(typeof test); // undefined
+console.log(test); // undefined
