3dc08246a6a7b9c930e4c9104ab97f702913eddb
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-05-26 12:41:56 +0530

    Arrays in TS

---
+++ 05-arrays.ts
@@ -0,0 +1,11 @@
+/************************************************
+ * ARRAYS IN TYPESCRIPT
+*/
+
+let numArr: number[] = [23, 34, 56];
+let person = ['Shani', 24, true, {
+  name: 'Shani'
+}];
+let birthYear = [1980, 1997, 1999]; // Automatic Type Inferance
+
+let mixedData: (string | number | object)[] = [];
