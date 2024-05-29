443626522cea4f9bf1fde7d03c0b706f55c10d63
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-05-29 14:47:54 +0530

    Function return type

---
+++ 12-funcreturntype.ts
@@ -0,0 +1,15 @@
+/********************************************************
+ * FUNCTION RETURN TYPE IN TS
+*/
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+console.log(sum(12, 23));
+
+function clg(msg: string): void {
+  console.log(msg);
+}
+
+console.log(clg('Hey'));
