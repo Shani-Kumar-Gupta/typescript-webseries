4982785db51eab1d82f34a025ab92002aa092515
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2024-05-29 14:18:39 +0530

    Literal Type in TS

---
+++ 10-literaltype.ts
@@ -0,0 +1,12 @@
+/****************************************************
+ * LITERAL TYPE IN TS
+ * Exactly what value will store it in the variable
+ * The value itself is assigned as variable data type.
+*/
+
+const str = 'Hey! TypeScript'; // Literal Type
+let str2 = 'Hey! JavaScript';
+let role: 'admin' | 'read' | 'read-write'; // Literal Type
+
+// role = 'abc';
+role = 'admin';
